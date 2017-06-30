4c33d561840d1d57fedf52360eb6d6fbc305030a
Bastien <bastien_du_21@hotmail.fr>
2017-06-30 14:28:14 +0200

    Ajout des controlleurs dans l'API

---
+++ ProjetWeb.API/Controllers/ReservationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjetWeb.BL;
+using ProjetWeb.Model;
+
+namespace ProjetWeb.API.Controllers
+{
+    public class ReservationController : Controller
+    {
+        // on instancie un ReservationBL pour utiliser les méthodes
+        private ReservationBL BLReservation = new ReservationBL();
+        // GET: Utilisateur
+        public List<ReservationModel> Get()
+        {
+            //On appelle la fonction GetLesReservations
+            return BLReservation.GetLesReservation();
+        }
+        public ReservationModel Get(int id)
+        {
+            //On appelle la fonction GetUnUtilisateurById
+            return BLReservation.GetUneReservationById(id);
+        }
+    }
+}
---
+++ ProjetWeb.API/Controllers/RessourceController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjetWeb.BL;
+using ProjetWeb.Model;
+
+namespace ProjetWeb.API.Controllers
+{
+    public class RessourceController : Controller
+    {
+        // on instancie un RessourceBL pour utiliser les méthodes
+        private RessourceBL BLRessource = new RessourceBL();
+        // GET: Utilisateur
+        public List<RessourceModel> Get()
+        {
+            //On appelle la fonction GetLesRessources
+            return BLRessource.GetLesRessource();
+        }
+        public RessourceModel Get(int id)
+        {
+            //On appelle la fonction GetUnRessourceById
+            return BLRessource.GetUneRessourceById(id);
+        }
+    }
+}
