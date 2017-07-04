b51b58d6f7b53cc39282b0884e5f9ae16e1d1083
Bastien <bastien_du_21@hotmail.fr>
2017-07-04 14:13:10 +0200

    Modif contrôleur Resa

--- ProjetWeb.WEB/Controllers/ReservationsController.cs
+++ ProjetWeb.WEB/Controllers/ReservationsController.cs
@@ -46,89 +46,93 @@ namespace ProjetWeb.WEB.Controllers
             return View(reservation);
         }
 
-        // GET: Reservations/Create
-        //public ActionResult Create()
-        //{
-        //    ViewBag.utilisateur_id = new SelectList(db.Utilisateur, "id", "nom");
-        //    return View();
-        //}
+        //GET: Reservations/Create
+        public ActionResult Create()
+        {
+            //ViewBag.utilisateur_id = new SelectList(db.Utilisateur, "id", "nom");
+            return View();
+        }
 
-        //// POST: Reservations/Create
-        //// Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        //// plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "id,utilisateur_id,date_resa,purge")] ReservationModel reservation)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Reservation.Add(reservation);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
+        // POST: Reservations/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,utilisateur_id,date_resa,purge")] ReservationModel reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                BLResa.CreateReservation(reservation.id,reservation.utilisateur_id,reservation.date_resa,reservation.purge);
+                //db.Reservation.Add(reservation);
+                //db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-        //    ViewBag.utilisateur_id = new SelectList(db.Utilisateur, "id", "nom", reservation.utilisateur_id);
-        //    return View(reservation);
-        //}
+            //ViewBag.utilisateur_id = new SelectList(db.Utilisateur, "id", "nom", reservation.utilisateur_id);
+            return View(reservation);
+        }
 
-        //// GET: Reservations/Edit/5
-        //public ActionResult Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Reservation reservation = db.Reservation.Find(id);
-        //    if (reservation == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    ViewBag.utilisateur_id = new SelectList(db.Utilisateur, "id", "nom", reservation.utilisateur_id);
-        //    return View(reservation);
-        //}
+        // GET: Reservations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ReservationModel reservation = BLResa.GetUneReservationById(id.GetValueOrDefault());
+            if (reservation == null)
+            {
+                return HttpNotFound();
+            }
+            //ViewBag.utilisateur_id = new SelectList(db.Utilisateur, "id", "nom", reservation.utilisateur_id);
+            return View(reservation);
+        }
 
         //// POST: Reservations/Edit/5
         //// Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
         //// plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
         //[HttpPost]
         //[ValidateAntiForgeryToken]
-        //public ActionResult Edit([Bind(Include = "id,utilisateur_id,date_resa,purge")] Reservation reservation)
+        //public ActionResult Edit([Bind(Include = "id,utilisateur_id,date_resa,purge")] ReservationModel reservation)
         //{
         //    if (ModelState.IsValid)
         //    {
-        //        db.Entry(reservation).State = EntityState.Modified;
-        //        db.SaveChanges();
+        //        BLResa.ModifierReservation
+        //        //db.Entry(reservation).State = EntityState.Modified;
+        //        //db.SaveChanges();
         //        return RedirectToAction("Index");
         //    }
-        //    ViewBag.utilisateur_id = new SelectList(db.Utilisateur, "id", "nom", reservation.utilisateur_id);
+        //    //ViewBag.utilisateur_id = new SelectList(db.Utilisateur, "id", "nom", reservation.utilisateur_id);
         //    return View(reservation);
         //}
 
-        //// GET: Reservations/Delete/5
-        //public ActionResult Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Reservation reservation = db.Reservation.Find(id);
-        //    if (reservation == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(reservation);
-        //}
+        // GET: Reservations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BLResa.DeleteReservation(id.GetValueOrDefault());
+            //Reservation reservation = db.Reservation.Find(id);
+            if (BLResa == null)
+            {
+                return HttpNotFound();
+            }
+            return View();
+        }
 
-        //// POST: Reservations/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult DeleteConfirmed(int id)
-        //{
-        //    Reservation reservation = db.Reservation.Find(id);
-        //    db.Reservation.Remove(reservation);
-        //    db.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
+        // POST: Reservations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BLResa.DeleteReservation(id);
+            //Reservation reservation = db.Reservation.Find(id);
+            //db.Reservation.Remove(reservation);
+            //db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
         //protected override void Dispose(bool disposing)
         //{
